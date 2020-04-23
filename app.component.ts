3c89852231d2dcaa9556f2350e6757c0517c710b
anjalidesh <64198302+anjalidesh@users.noreply.github.com>
2020-04-23 15:45:47 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+// import * as videojs from 'video.js'
+ 
+ 
+ /**export interface IMedia {
+   title: string;
+   src: string;
+   type: string;**/
+
+declare var video: any;
+declare var my_video_1: any;
+declare var transcript: any;
+
+
+
+
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+//export class AppComponent implements OnInit, OnDestroy {
+	export class AppComponent {
+   
+}
+      
+ 
+		 
+	
+	
